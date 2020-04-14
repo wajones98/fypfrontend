3edb48869665bc908d70060da5256bea287ded5a
wajsters <32671145+wajsters@users.noreply.github.com>
2020-04-14 10:49:30 +0100

    Login api call and storing session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
+import Login from 'components/Login';
+
 
 function App() {
+
   return (
     <div className="App">
-      <NavBar />
+      <Login/>
     </div>
   );
 }
